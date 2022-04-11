e01d2ff7cb0e50498de0772a6d7188ac3c96ac6b
Diego Esquivel <mpi.diegoe@gmail.com>
2022-04-11 16:41:32 -0500

    Add initial files to github repo

---
+++ middleware.h
@@ -0,0 +1,6 @@
+/********************************************************************************************************************************************
+ *  Author: Diego G Esquivel & Eric D Smith
+ *  CSCE306 Spring 2022 - Object Oriented Software Development
+ *  Portfolio Project v.0.1: - esquivel_diego_labfinal.cpp, <iostream>, <string>, <array>
+ *  Code Summary: This source code file demonstrates our class learned materail.
+ * *****************************************************************************************************************************************/
