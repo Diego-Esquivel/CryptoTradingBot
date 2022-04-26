117daea1d87c072bb604b12fcc845e08e5dfe6a9
Diego Esquivel <mpi.diegoe@gmail.com>
2022-04-26 15:56:25 -0500

    Update to files

--- cryptoBot/cryptoBot.js
+++ cryptoBot/cryptoBot.js
@@ -1,7 +1,13 @@
 document.getElementById("yes").addEventListener("click",function(){
-		
+	if(window.addEventListener) 
+	window.addEventListener("click",() => {document.location.href = "http://127.0.0.1:126";});
+	else if(window.attachEvent) 
+	window.attachEvent("onclick", () => {document.location.href = "https://127.0.0.1:126";});
 
 });
 document.getElementById("no").addEventListener("click",function(){
-	
-	//deals cards to the user	
+	if(window.addEventListener) 
+        window.addEventListener("load",() => {document.location.href = "http://127.0.0.1:126";});
+    else if(window.attachEvent) 
+        window.attachEvent("onload", () => {document.location.href = "https://127.0.0.1:126";});
+});
